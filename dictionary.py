3e26626bfed0656e7a1a12a1720208023891df84
hrikhan <hridoykhan6786@gmail.com>
2025-09-07 12:25:07 +0600

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+person = {
+    'name': 'John Doe',
+    'age': 30,
+    'city': 'New York'
+}
+
+print(person['name'])
+print(person['age'])
+print(person['city'])
+
+person['age'] = 31
+
+print(person['age'])
+print(person['age'])
---
+++ set.py
@@ -0,0 +1,11 @@
+number = {1, 2, 3, 4, 5}
+print(number)
+
+number.add(1000)
+print(number)
+
+number.add(6)
+print(number)
+
+number.remove(6)
+print(number)
